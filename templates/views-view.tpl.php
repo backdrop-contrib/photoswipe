612f707059a8c6e68664e155fbb24de37270f6f9
Graham Oliver <g.oliver@btinternet.com>
2021-03-22 08:01:03 +0000

    Conditions for applying modified template. (#20)
    * Additional styling for gallery default-skin.
    
    * README updated.
    
    * Check for image field of type photoswipe.
    
    * Prevent clash with style plugins.
    
    Co-authored-by: Graham-72 <graham220942@gmail.com>

--- templates/views-view.tpl.php
+++ templates/views-view.tpl.php
@@ -24,7 +24,25 @@
  *
  * @ingroup views_templates
  */
+
+/**
+ * Modify template for views that use photoswipe.
+ * Check view config settings for image field type 'photoswipe'.
+ */
+$view_name = $variables['name'];
+$config_name = 'views.view.' . $view_name;
+$settings = config_get($config_name,'display.default.display_options.fields');
+$field_name = array();
+$view_type = '';
+$div_class = 'view-content';
+if($settings['style_plugin'] == 'default') {
+  foreach($settings['fields'] as $field_name ) {
+    if(array_key_exists('type', $field_name)) {$image_type = $field_name['type'];}
+    if($image_type == 'photoswipe'){$div_class = 'view-content photoswipe-gallery'; }
+  }
+}
 ?>
+
 <div class="<?php print implode(' ', $classes); ?>">
   <?php print render($title_prefix); ?>
   <?php if ($title): ?>
@@ -50,7 +68,7 @@
   <?php endif; ?>
 
   <?php if ($rows): ?>
-    <div class="view-content photoswipe-gallery">
+    <div class=" <?php print $div_class; ?> ">
       <?php print (is_array($rows)) ? backdrop_render($rows) : $rows; ?>
     </div>
   <?php elseif ($empty): ?>
