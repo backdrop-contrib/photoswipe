f0ad73c6f4e73add5e58d33baa09fafcce54eafb
Graham-72 <graham220942@gmail.com>
2021-03-22 07:45:52 +0000

    Prevent clash with style plugins.

--- templates/views-view.tpl.php
+++ templates/views-view.tpl.php
@@ -35,9 +35,11 @@ $settings = config_get($config_name,'display.default.display_options.fields');
 $field_name = array();
 $view_type = '';
 $div_class = 'view-content';
-foreach($settings as $field_name ) {
-  if(array_key_exists('type', $field_name)) {$image_type = $field_name['type'];}
-  if($image_type == 'photoswipe'){$div_class = 'view-content photoswipe-gallery'; }
+if($settings['style_plugin'] == 'default') {
+  foreach($settings['fields'] as $field_name ) {
+    if(array_key_exists('type', $field_name)) {$image_type = $field_name['type'];}
+    if($image_type == 'photoswipe'){$div_class = 'view-content photoswipe-gallery'; }
+  }
 }
 ?>
 
